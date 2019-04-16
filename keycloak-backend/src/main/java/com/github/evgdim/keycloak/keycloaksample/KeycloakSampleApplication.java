a8f238201ba9e32c06e1c7952bef43f63c07597b
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2019-04-16 21:12:29 +0300

    initial

---
+++ keycloak-backend/src/main/java/com/github/evgdim/keycloak/keycloaksample/KeycloakSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.evgdim.keycloak.keycloaksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSampleApplication.class, args);
+	}
+
+}
