8ce9a81211dbdee6492d3cbae6ddd545b80ccb35
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2019-04-18 16:08:34 +0300

    add keycloak security with servlet filter

--- keycloak-backend/src/main/java/com/github/evgdim/keycloak/keycloaksample/KeycloakSampleApplication.java
+++ keycloak-backend/src/main/java/com/github/evgdim/keycloak/keycloaksample/KeycloakSampleApplication.java
@@ -1,7 +1,10 @@
 package com.github.evgdim.keycloak.keycloaksample;
 
+import org.keycloak.adapters.servlet.KeycloakOIDCFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KeycloakSampleApplication {
@@ -10,4 +13,15 @@ public class KeycloakSampleApplication {
 		SpringApplication.run(KeycloakSampleApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean keycloakFilter() {
+		FilterRegistrationBean<KeycloakOIDCFilter> registrationBean
+				= new FilterRegistrationBean<>();
+		KeycloakOIDCFilter filter = new KeycloakOIDCFilter();
+
+		registrationBean.setFilter(filter);
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.addInitParameter("keycloak.config.file", "D:\\PROJECTS\\GitHub\\keycloak-sample\\keycloak-backend\\src\\main\\resources\\keycloak.json");
+		return registrationBean;
+	}
 }
--- keycloak-backend/src/main/java/com/github/evgdim/keycloak/keycloaksample/controller/MainController.java
+++ keycloak-backend/src/main/java/com/github/evgdim/keycloak/keycloaksample/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.github.evgdim.keycloak.keycloaksample.controller;
 
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.servlet.OIDCServletHttpFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/")
 public class MainController {
     private final static Logger logger = LoggerFactory.getLogger(MainController.class);
-    @GetMapping
+    @GetMapping("/test")
     public String index(HttpServletRequest req) {
         Map<String, String> headers = Collections.list(req.getHeaderNames())
                 .stream()
@@ -26,6 +29,9 @@ public class MainController {
                         )
                 );
         logger.info("headers: {}", headers.entrySet().stream().map(e -> e.getKey() + "=" + e.getValue()).collect(Collectors.joining("\n")));
+
+        KeycloakSecurityContext securityContext = (KeycloakSecurityContext) req.getAttribute(KeycloakSecurityContext.class.getName());
+        logger.info("KeycloakSecurityContext: {}", securityContext);
         return "index";
     }
 }
